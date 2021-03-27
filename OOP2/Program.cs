5ea68b82ab1e59b44a935e3ce5114d9d566aadbf
İbrahim KARADAĞ <56786042+KodcuKrdg@users.noreply.github.com>
2021-03-27 11:05:38 +0300

    Kamp5

---
+++ OOP2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.TcNo = "123345678910";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi="Kodlama.io";
+            musteri2.Id = 2;
+            
+        }
+    }
+}
