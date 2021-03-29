3eed24c39e56c5e3a4c4c28352dd270fed15c2db
İbrahim KARADAĞ <56786042+KodcuKrdg@users.noreply.github.com>
2021-03-29 17:55:18 +0300

    5. Kamp

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -7,6 +7,7 @@ namespace OOP2
         static void Main(string[] args)
         {
             GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
             musteri1.MusteriNo = "12345";
             musteri1.Adi = "Engin";
             musteri1.Soyadi = "Demiroğ";
@@ -16,8 +17,15 @@ namespace OOP2
             musteri2.Id = 2;
             musteri2.MusteriNo = "54321";
             musteri2.SirketAdi="Kodlama.io";
+            musteri2.VergiNo = "123345567";
             musteri2.Id = 2;
-            
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
         }
     }
 }
---
+++ OOP3/FileLoggerSevise.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP3
+{
+    class FileLoggerSevise : ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("Dosyaya loglandı");
+        }
+    }
+}
