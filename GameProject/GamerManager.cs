12f674282e4d19ba14879a9e814e7eeaa23fba1c
İbrahim KARADAĞ <56786042+KodcuKrdg@users.noreply.github.com>
2021-04-03 15:04:33 +0300

    Kamp 6

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    class GamerManager : IGamerServise
+    {
+        IUserValidation _userValidation;
+
+        public GamerManager(IUserValidation userValidation)
+        {
+            _userValidation = userValidation;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (_userValidation.Validate(gamer))
+            {
+                Console.WriteLine("Kayıt oldu");
+            }
+            else
+            {
+                Console.WriteLine("Doğrulama başarısız");
+            }
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Silin");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Güncellendi");
+        }
+    }
+}
